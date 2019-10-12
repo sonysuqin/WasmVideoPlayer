9241a675589405a4a63e9771cdc5c2eef092e6a0
sonysuqin <sonysuqin@gmail.com>
2019-10-12 10:49:19 +0800

    Format indentation and CRLF.

--- build_decoder.sh
+++ build_decoder.sh
@@ -4,10 +4,10 @@ mkdir -p dist
 cd ../ffmpeg
 make clean
 emconfigure ./configure --cc="emcc" --cxx="em++" --ar="emar" --prefix=$(pwd)/../WasmVideoPlayer/dist --enable-cross-compile --target-os=none \
-		--arch=x86_32 --cpu=generic --enable-gpl --enable-version3 --disable-avdevice --disable-swresample --disable-postproc --disable-avfilter \
-		--disable-programs --disable-logging --disable-everything --enable-avformat --enable-decoder=hevc --enable-decoder=h264 --enable-decoder=aac \
-		--disable-ffplay --disable-ffprobe --disable-ffserver --disable-asm --disable-doc --disable-devices --disable-network --disable-hwaccels \
-		--disable-parsers --disable-bsfs --disable-debug --enable-protocol=file --enable-demuxer=mov --disable-indevs --disable-outdevs
+        --arch=x86_32 --cpu=generic --enable-gpl --enable-version3 --disable-avdevice --disable-swresample --disable-postproc --disable-avfilter \
+        --disable-programs --disable-logging --disable-everything --enable-avformat --enable-decoder=hevc --enable-decoder=h264 --enable-decoder=aac \
+        --disable-ffplay --disable-ffprobe --disable-ffserver --disable-asm --disable-doc --disable-devices --disable-network --disable-hwaccels \
+        --disable-parsers --disable-bsfs --disable-debug --enable-protocol=file --enable-demuxer=mov --disable-indevs --disable-outdevs
 make
 make install
 cd ../WasmVideoPlayer
--- build_decoder_wasm.sh
+++ build_decoder_wasm.sh
@@ -1,8 +1,8 @@
-rm -rf libffmpeg.wasm libffmpeg.js 
+rm -rf libffmpeg.wasm libffmpeg.js
 export TOTAL_MEMORY=67108864
 export EXPORTED_FUNCTIONS="[ \
-		'_initDecoder', \
-		'_uninitDecoder', \
+    '_initDecoder', \
+    '_uninitDecoder', \
     '_openDecoder', \
     '_closeDecoder', \
     '_sendData', \
@@ -16,10 +16,10 @@ emcc decoder.c dist/lib/libavformat.a dist/lib/libavcodec.a dist/lib/libavutil.a
     -I "dist/include" \
     -s WASM=1 \
     -s TOTAL_MEMORY=${TOTAL_MEMORY} \
-   	-s EXPORTED_FUNCTIONS="${EXPORTED_FUNCTIONS}" \
-   	-s EXTRA_EXPORTED_RUNTIME_METHODS="['addFunction']" \
-		-s RESERVED_FUNCTION_POINTERS=14 \
-		-s FORCE_FILESYSTEM=1 \
+    -s EXPORTED_FUNCTIONS="${EXPORTED_FUNCTIONS}" \
+    -s EXTRA_EXPORTED_RUNTIME_METHODS="['addFunction']" \
+    -s RESERVED_FUNCTION_POINTERS=14 \
+    -s FORCE_FILESYSTEM=1 \
     -o libffmpeg.js
 
 echo "Finished Build"
