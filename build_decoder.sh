19a718d24468f8a139eb2a57d83bcc4658f5dca0
sonysuqin <sonysuqin@gmail.com>
2020-01-20 09:39:53 +0800

    fix #23: Ubuntu build error.

--- build_decoder.sh
+++ build_decoder.sh
@@ -2,16 +2,16 @@ echo "Beginning Build:"
 rm -r dist
 mkdir -p dist
 cd ../ffmpeg
-if [ -f "Makefile" ]; then
-  echo "make clean"
-  make clean
-fi
 echo "emconfigure"
-emconfigure ./configure --cc="emcc" --cxx="em++" --ar="emar" --prefix=$(pwd)/../WasmVideoPlayer/dist --enable-cross-compile --target-os=none \
+emconfigure ./configure --cc="emcc" --cxx="em++" --ar="emar" --ranlib="emranlib" --prefix=$(pwd)/../WasmVideoPlayer/dist --enable-cross-compile --target-os=none \
         --arch=x86_32 --cpu=generic --enable-gpl --enable-version3 --disable-avdevice --disable-swresample --disable-postproc --disable-avfilter \
         --disable-programs --disable-logging --disable-everything --enable-avformat --enable-decoder=hevc --enable-decoder=h264 --enable-decoder=aac \
         --disable-ffplay --disable-ffprobe --disable-ffserver --disable-asm --disable-doc --disable-devices --disable-network --disable-hwaccels \
         --disable-parsers --disable-bsfs --disable-debug --enable-protocol=file --enable-demuxer=mov --enable-demuxer=flv --disable-indevs --disable-outdevs
+if [ -f "Makefile" ]; then
+  echo "make clean"
+  make clean
+fi
 echo "make"
 make
 echo "make install"
